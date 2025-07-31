7198cf5f38ba6aea568001222cb41c9a69ef3c62
Aashish Kumar Jha <114846931+Jhaempyre@users.noreply.github.com>
2025-07-31 13:57:20 +0530

    Create connectomachine.sh
    connection holded

---
+++ First_time_on_the_machine./connectomachine.sh
@@ -0,0 +1,4 @@
+ssh -i /home/kali/.ssh/appmastercluster libdata_ld@34.61.6.233
+## in the fromat ssh -i (location of privatekeys file) username@IPaddress
+
+## if passphrase were added then pass it or you are connceted boom 
