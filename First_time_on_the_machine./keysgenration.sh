ac84a02d9bf8980192809d4f7659ced2385dd816
Aashish Kumar Jha <114846931+Jhaempyre@users.noreply.github.com>
2025-07-31 13:55:16 +0530

    Create keysgenration.sh
    keygenrationadded

---
+++ First_time_on_the_machine./keysgenration.sh
@@ -0,0 +1,5 @@
+ssh-keygen -t ed25519 -C "libdata.ld@gmail.com" ##you can add your email here
+##it will ask for which file to save the file , /home/kali/.ssh/appmastercluster something like this then 
+##hit enter 
+##Enter passphrase just password if you want otherwise hit enter
+##your keys are now saved
