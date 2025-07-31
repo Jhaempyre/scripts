ae7d744ddb4bb9e46238e7bd3fb55aeef5a4067c
Aashish Kumar Jha <114846931+Jhaempyre@users.noreply.github.com>
2025-07-31 14:05:32 +0530

    Create justonthemachine.sh
    on the machine added

---
+++ First_time_on_the_machine./justonthemachine.sh
@@ -0,0 +1,16 @@
+## when you are on the machine you should not work as root but instead you should create a user with sudo preivelage just like this 
+
+sudo adduser username
+
+##it will prompt for password provide it and then 
+sudo usermod -aG sudo 
+##it adds sudo privelages 
+
+#Hold on login with this username 
+ su - username
+ ##add the password ....
+
+
+ ## now do 
+ sudo apt update 
+ sudo apt upgrade
