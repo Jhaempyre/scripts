0846c83d8c0947e5663127db142b47cdc1b76341
Aashish Kumar Jha <114846931+Jhaempyre@users.noreply.github.com>
2026-02-23 17:59:06 +0530

    Update gpgkey.sh to include Git user configuration
    Added commands to configure Git user details and verify settings.

--- daytodayscripts/gpgkey.sh
+++ daytodayscripts/gpgkey.sh
@@ -15,9 +15,22 @@ git config --global commit.gpgsign true
 echo 'export GPG_TTY=$(tty)' >> ~/.bashrc
 source ~/.bashrc
 
+
 ##run 
 
 echo "test" | gpg --clearsign
+
+##add 
+
+git config --global user.name "Your New Name"
+git config --global user.email "yournewemail@example.com"
+
+## chek here if all correct 
+
+git config --global user.name
+git config --global user.email
+
+
 
 gpg --armor --export <YOUR_GPG_KEY_ID>
 ##compy whole block and add it into github gpg keys "
