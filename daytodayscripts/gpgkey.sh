21c0a3a873dbad5321fd10dbb59d15565b0e349e
Aashish Kumar Jha <114846931+Jhaempyre@users.noreply.github.com>
2026-02-23 17:57:25 +0530

    Add GPG key setup script
    This script installs GnuPG, generates a GPG key, configures Git to use the key for signing commits, and updates the bash configuration for GPG.

---
+++ daytodayscripts/gpgkey.sh
@@ -0,0 +1,25 @@
+sudo apt-get install gnupg
+gpg --full-generate-key
+## see the propmpts fill the details exact as on github
+
+gpg --list-secret-keys --keyid-format=long
+
+##From the output, copy the long form of the GPG key ID (the 16-character string after the / in the sec line).
+
+git config --global user.signingkey <YOUR_GPG_KEY_ID>
+
+git config --global commit.gpgsign true
+
+##make it permanet by entring in bashscript 
+
+echo 'export GPG_TTY=$(tty)' >> ~/.bashrc
+source ~/.bashrc
+
+##run 
+
+echo "test" | gpg --clearsign
+
+gpg --armor --export <YOUR_GPG_KEY_ID>
+##compy whole block and add it into github gpg keys "
+
+##now commit .happy
