278ff90cd71c78505fa986b467db6925190a266e
Aashish Kumar Jha <114846931+Jhaempyre@users.noreply.github.com>
2026-07-08 17:59:37 +0530

    Correct typo in Docker command
    Fix typo in command from 'doceker' to 'docker'.

--- Docker/installation.sh
+++ Docker/installation.sh
@@ -2,7 +2,7 @@ sudo apt install docker.io
 
 ##give password 
 
-doceker ps 
+docker ps 
 ## will throw this error of permission denied 
 ##permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock: Get "http://%2Fvar%2Frun%2Fdocker.sock/v1.47/containers/json": dial unix /var/run/docker.sock: connect: permission denied
 
